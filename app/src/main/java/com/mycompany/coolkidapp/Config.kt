dac4ff3559412bcc0963436fef329f2fd1109f44
kumarshreyak@gmail.com <shreyak.kumar@novopay.in>
2020-04-22 20:47:38 +0530

    added youtube player api.

---
+++ app/src/main/java/com/mycompany/coolkidapp/Config.kt
@@ -0,0 +1,7 @@
+package com.mycompany.coolkidapp
+
+class Config {
+    companion object {
+        val YOUTUBE_API_KEY = "AIzaSyAQEdw2wOTswvT-IWVxDJl3h9U_51VPvKI"
+    }
+}
--- app/src/main/java/com/mycompany/coolkidapp/MainActivity.kt
+++ app/src/main/java/com/mycompany/coolkidapp/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.mycompany.coolkidapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.mycompany.coolkidapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initView()
     }
+
+    private fun initView() {
+        binding.ytView.initialize(Config.YOUTUBE_API_KEY, this)
+    }
+
+    override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?,
+        wasRestored: Boolean) {
+        if (!wasRestored) {
+            player?.cueVideo("q6uuw0wwCgU"); // Plays https://www.youtube.com/watch?v=q6uuw0wwCgU
+        }
+    }
+
+    override fun onInitializationFailure(provider: YouTubePlayer.Provider?,
+        errorReason: YouTubeInitializationResult?) {
+        Toast.makeText(this, errorReason.toString(), Toast.LENGTH_SHORT).show()
+    }
+
 }
